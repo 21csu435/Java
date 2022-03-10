aadaeb071205550bb4830ebcba0a9e75d8e29ee6
21csu435 <101317444+21csu435@users.noreply.github.com>
2022-03-10 14:10:26 +0530

    Add files via upload

---
+++ maths.java
@@ -0,0 +1,10 @@
+class Maths
+{public static void main(String[]args)
+	{int num1 = 6;
+	 int num2 = 3;
+	 int add = num1 + num2;
+	 int subtraction = num1 - num2;
+	 System.out.println(add);
+	 System.out.println(subtraction);
+	 }
+}
