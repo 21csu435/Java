8c67ef227f779bcb85c1d30b50089338fd86b5db
21csu435 <101317444+21csu435@users.noreply.github.com>
2022-03-11 10:50:34 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,14 @@
+class calculator
+{public static void main(String[]args)
+	{int num1 = 6;
+	 int num2 = 3;
+	 int a = num1 + num2;
+	 int s = num1 - num2;
+	 int m = num1 * num2;
+	 int d = num1/num2;
+	 System.out.println(a);
+	 System.out.println(s);
+	 System.out.println(m);
+	 System.out.println(d);
+	 }
+}
